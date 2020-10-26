4a187595ef1369a0af63a2404c20ae5df8e6a12e
apple <apple@appledeMacBook-Pro.local>
2020-10-26 15:30:25 +0800

    use WEBform

--- hello.py
+++ hello.py
@@ -1,12 +1,35 @@
 from flask import Flask,render_template
+from flask_bootstrap import Bootstrap 
+from flask_moment import Moment 
+from flask_wtf import FlaskForm 
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired 
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
-@app.route('/')
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+class NameForm(FlaskForm):
+	name = StringField('What's your name?,validators=[DataRequired()])
+	submit = SubmitField('Submit')
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'),404
+
+@app.errorhandler(500)
+def page_not_found(e):
+	return render_template('500.html'),500
+
+@app.route('/',methods=['GET','POST'])
 def index():
-	return render_template('index.html')
-
-@app.route('/user/<name>')
-def user(name):
-	return render_template('user.html',name=name)
-
+	name = None
+	from = NameForm()
+	if form.validators_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('index.html',form=form,name=name)
+	
