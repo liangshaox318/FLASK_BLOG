8959c961ee63f0b8b0eeca1f4e6198b9f9473972
Liangshaoxiong <liangshaox318@outlook.com>
2021-02-22 19:41:56 +0800

    little modify

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -11,7 +11,6 @@ auth = HTTPBasicAuth()
 def verify_password(email_or_token,password):
 	if email_or_token == '':
 		return False
-	user = User.query.filter_by(email=email_or_token).first()
 	if password == '':
 		g.current_user = User.verify_auth_token(email_or_token)
 		g.token_used = True
